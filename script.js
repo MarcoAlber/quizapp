4eab1116ea723e70879f0250f5717435af2aa81c
Marco Alber <m.alber@onlinehome.de>
2023-01-31 22:56:20 +0100

    implement questions

---
+++ script.js
@@ -0,0 +1,50 @@
+let questions = [
+    {
+        "question": "Seit wann gibt es Menschen (Homo sapiens)?",
+        "answer1": "30.000 Jahren",
+        "answer2": "300.000 Jahren",
+        "answer3": "3.000 Jahren",
+        "answer4": "3.000.000 Jahren",
+        "rightAnswer": 2
+    },
+    {
+        "question": "Welcher ist der zweitgrößte Planet unseres Sonnensystems?",
+        "answer1": "Venus",
+        "answer2": "Neptun",
+        "answer3": "Jupiter",
+        "answer4": "Saturn",
+        "rightAnswer": 4
+    },
+    {
+        "question": "Wie oft passt Deutschland flächenmäßig in Russland?",
+        "answer1": "15",
+        "answer2": "33",
+        "answer3": "25",
+        "answer4": "47",
+        "rightAnswer": 4
+    },
+    {
+        "question": "Haben Rosen Stacheln oder Dornen?",
+        "answer1": "Stacheln",
+        "answer2": "Dornen",
+        "answer3": "Ist beides das Gleiche",
+        "answer4": "Keines von beiden",
+        "rightAnswer": 1
+    },
+    {
+        "question": "Wie groß war der größte Mensch der Welt?",
+        "answer1": "3,19m",
+        "answer2": "2,44m",
+        "answer3": "2,72m",
+        "answer4": "2,97m",
+        "rightAnswer": 3
+    },
+    {
+        "question": "Wie viele Schritte brauchte Usain Bolt bei seinem Weltrekord über 100m?",
+        "answer1": "41",
+        "answer2": "48",
+        "answer3": "56",
+        "answer4": "70",
+        "rightAnswer": 1
+    }
+];
